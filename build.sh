1d372ae9f885081464faff2fd9688e44203b1bbc
Jennifer Jones <jybjones@gmail.com>
2015-05-14 12:17:43 -0500

    a whole lot of changes based off of scott's

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-#install dependencies
 npm install
 bower install
 
@@ -9,32 +8,26 @@ rm -rf public
 cp -r src public
 
 # compile jade to html
-./node_modules/.bin/jade src -P
-cd src 
-find  . -name "*.html" | cpio -pdvm ../public
-cd ..
-rm -rf src/*.html \
-     src/**/*.html \
-      public/**/_*.html \
-      public/_partials
+./node_modules/.bin/jade src -o public -PH
+rm -rf public/_partials
 
-#compile sass to css
+# compile sass to css
 ./node_modules/.bin/node-sass \
---output-style compressed \
---source-map-embed \
-src/_styles/main.scss public/css/main.css
+  --output-style compressed \
+  --source-map-embed \
+  src/_styles/main.scss public/css/main.css
 
-# convert ES6 to JS to ES5
+# convert ES6 JS to ES5
 ./node_modules/.bin/babel src --out-dir public -s inline
 
-# concat bower_components to lib directory
-if [ -d "bower_components" ]; then 
-./node_modules/.bin/bowcat . -o public/lib -m
-fi 
+# concat bower_components to public/lib directory
+if [ -d "bower_components" ]; then
+  ./node_modules/.bin/bowcat . -o public/lib -m
+fi
 
 # clean unneeded files
 rm -rf public/_styles \
-    public/*.jade \
-    public/**/*.jade \
-    public/*.scss \
-    public/**/*.scss 
+       public/*.jade \
+       public/**/*.jade \
+       public/*.scss \
+       public/**/*.scss
